7d1c5a5bed2da328725435e43bc59665e9986676
Olivia Appleton <olivia.kay.appleton@gmail.com>
2018-11-29 10:46:31 -0800

    Solve 47, revise 7 and 8 in the process

--- python/p0007.py
+++ python/p0007.py
@@ -11,6 +11,10 @@ Revision 1:
 Add a shortcut on is_prime to check the pfs cache first, and refactored to work
 with the new pfs object and name
 
+Revision 2:
+
+Add a count to the trigger on prime numbers
+
 Problem:
 
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
@@ -19,19 +23,30 @@ the 6th prime is 13.
 What is the 10 001st prime number?
 """
 from itertools import chain, count
-from typing import Iterator
+from typing import Iterator, Optional, Set
 
 from p0003 import prime_factors
 
 
-def is_prime(num: int) -> bool:
+def is_prime(num: int, count: int = 1, distinct: bool = False) -> bool:
+    """Detects if a number is prime or not.
+
+    If a count other than 1 is given, it returns True only if the number has
+    exactly count prime factors. The distinct flag will have it look only for
+    unique prime factors."""
     if num in (0, 1):
         return False
-    if num in prime_factors.cache:
-        return True
     factors = iter(prime_factors(num))
-    next(factors)
-    return next(factors, None) is None
+    if count == 1:
+        if num in prime_factors.cache:
+            return True
+        next(factors)
+        return next(factors, None) is None
+    else:
+        seen: Set[Optional[int]] = set()
+        while None not in seen and count != len(seen):
+            seen.add(next(factors, None))
+        return None not in seen and next(factors, None) is None
 
 
 def primes() -> Iterator[int]:
---
+++ python/p0047.py
@@ -0,0 +1,49 @@
+"""
+Project Euler Problem 47
+
+I was able to chain this with a previous problem. Probably a suboptimal
+solution because of it, but it felt prettier this way.
+
+I was able to add a short-circuited fail case to the is_prime() method, though
+
+Problem:
+
+The first two consecutive numbers to have two distinct prime factors are:
+
+14 = 2 × 7
+15 = 3 × 5
+
+The first three consecutive numbers to have three distinct prime factors are:
+
+644 = 2² × 7 × 23
+645 = 3 × 5 × 43
+646 = 2 × 17 × 19.
+
+Find the first four consecutive integers to have four distinct prime factors
+each. What is the first of these numbers?
+"""
+from itertools import count
+from functools import lru_cache
+from typing import Tuple
+
+from p0003 import prime_factors
+from p0007 import is_prime
+from p0008 import groupwise
+
+
+@lru_cache()
+def cached_is_prime(args: Tuple[int, int, bool]) -> bool:
+    return is_prime(*args)
+
+
+def main() -> int:
+    for group in groupwise(count(2), 4):
+        print(group[0])
+        if all(cached_is_prime((x, 4, True)) for x in group):
+            # if all(len(set(prime_factors(x))) == 4 for x in group):
+            return group[0]
+    return -1
+
+
+if __name__ == '__main__':
+    print(main())
