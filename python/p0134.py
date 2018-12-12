a2c1751f25a92f2c6b0f81cb2522da3928aab4b6
Gabe Appleton <gabe@gabeappleton.me>
2018-12-11 23:28:50 -0800

    CRT

--- python/p0134.py
+++ python/p0134.py
@@ -6,6 +6,10 @@ reasonable runtime. I have made some minor optimizations in the hopes that it
 will be reduced further, but it only went down to 37 core-minutes. Not good
 enough.
 
+Revision 1:
+
+Found Chinese Remainder Theorem, applied it, got infinity x speed boost
+
 Problem:
 
 Consider the consecutive primes p1 = 19 and p2 = 23. It can be verified that
@@ -26,23 +30,39 @@ from p0007 import primes
 
 
 @cython.ccall
+@cython.returns(cython.ulonglong)
+def mul_inv(a: cython.ulonglong, b: cython.ulonglong):
+    """Multiplicative inverse for modulo numbers"""
+    if b == 1:
+        return 1
+    b0: cython.ulonglong = b
+    x0: cython.ulonglong = 0
+    x1: cython.ulonglong = 1
+    while a > 1:
+        q: cython.ulonglong = a // b
+        a, b = b, a % b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0:
+        return x1 + b0
+    return x1
+
+
+@cython.ccall
 @cython.locals(p2=cython.ulonglong, n=cython.ulonglong)
 @cython.returns(cython.ulonglong)
 def main() -> int:
     answer: cython.ulonglong = 0
     pow_10: cython.ulong = 10
-    iterator = primes(1000000)  # iterate over primes <1000000
+    iterator = primes(1000005)  # iterate over primes <1000005
     next(iterator)  # skip 2
     next(iterator)  # skip 3
     p1: cython.ulonglong = next(iterator)  # p1 = 5
     for p2 in iterator:  # 7, 11, 13, 17, ...
         while pow_10 < p1:
             pow_10 *= 10
-        for n in count(p2 * 3, p2 * 2):  # 3p2, 5p2, 7p2, ...
-            if n % pow_10 == p1:
-                answer += n
-                p1 = p2
-                break
+        answer += (p1 * p2 * mul_inv(p2, pow_10)) % (p2 * pow_10)
+        # simplified Chinese Remainder Theorem
+        p1 = p2
     return answer
 
 
