0ca90381bbbb0de943b487e7fc386399a50ea91d
Olivia Appleton <olivia.kay.appleton@gmail.com>
2018-12-11 23:29:57 -0800

    Revise these so that 134 actually works

--- python/p0007.py
+++ python/p0007.py
@@ -15,6 +15,10 @@ Revision 2:
 
 Add a count to the trigger on prime numbers
 
+Revision 3:
+
+Add a stop parameter to primes()
+
 Problem:
 
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
@@ -23,36 +27,53 @@ the 6th prime is 13.
 What is the 10 001st prime number?
 """
 from itertools import chain, count
-from typing import Iterator, Optional, Set
+from typing import Iterator, List, Optional, Set
+
+import cython
 
 from p0003 import prime_factors
 
 
-def is_prime(num: int, count: int = 1, distinct: bool = False) -> bool:
+@cython.final
+@cython.ccall
+@cython.returns(cython.bint)
+def is_prime(num: cython.ulonglong, count: cython.ulonglong = 1) -> bool:
     """Detects if a number is prime or not.
 
     If a count other than 1 is given, it returns True only if the number has
-    exactly count prime factors. The distinct flag will have it look only for
-    unique prime factors."""
+    exactly count prime factors."""
     if num in (0, 1):
         return False
     factors = iter(prime_factors(num))
     if count == 1:
-        if num in prime_factors.cache:
+        if num in prime_factors.cache:  # always has 2
             return True
+        elif num % 2 == 0:
+            return False
         next(factors)
         return next(factors, None) is None
     else:
-        seen: Set[Optional[int]] = set()
+        seen: List[Optional[cython.ulonglong]] = []
+        seen_add = seen.append
         while None not in seen and count != len(seen):
-            seen.add(next(factors, None))
+            seen_add(next(factors, None))
         return None not in seen and next(factors, None) is None
 
 
-def primes() -> Iterator[int]:
-    for num in chain((2, ), count(3, 2)):  # 2, then 3, 5, 7, 9, 11, 13, ...
-        if is_prime(num):
-            yield num
+@cython.final
+@cython.ccall
+@cython.locals(num=cython.ulonglong)
+def primes(stop=None) -> Iterator[int]:
+    if stop is None:
+        primary = prime_factors.cache
+    else:
+        primary = (x for x in prime_factors.cache if x < stop)
+    yield from primary
+    if stop is None:
+        secondary = count(prime_factors.last_cached, 2)
+    else:
+        secondary = range(prime_factors.last_cached, stop, 2)
+    yield from filter(is_prime, secondary)
 
 
 def main() -> int:
