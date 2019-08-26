485d064b1da732941d67302bfb9ef9f14447a81b
Gabe Appleton <gabe@gabeappleton.me>
2019-08-26 15:28:41 -0700

    Add 46

---
+++ python/p0046.py
@@ -0,0 +1,41 @@
+"""
+Project Euler Problem 46
+
+Problem:
+
+It was proposed by Christian Goldbach that every odd composite number can be written as the sum of a prime and twice a square.
+
+9 = 7 + 2×1^2
+15 = 7 + 2×2^2
+21 = 3 + 2×3^2
+25 = 7 + 2×3^2
+27 = 19 + 2×2^2
+33 = 31 + 2×1^2
+
+It turns out that the conjecture was false.
+
+What is the smallest odd composite that cannot be written as the sum of a prime and twice a square?
+"""
+from itertools import count
+
+from p0007 import is_prime, primes
+
+
+def main():
+    for goal in count(9, 2):
+        if is_prime(goal):
+            continue
+        for p in primes(goal):
+            done = False
+            for x in range(1, goal - p):
+                if p + 2 * x * x == goal:
+                    done = True
+                    break
+            if done:
+                break
+        else:
+            return goal
+
+
+if __name__ == '__main__':
+    print(main())
