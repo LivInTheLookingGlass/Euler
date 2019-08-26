106114a74f7f6773ef4b06141af077de7f1ee208
Olivia Appleton <olivia.kay.appleton@gmail.com>
2019-08-26 12:59:11 -0700

    Add 48

---
+++ python/p0048.py
@@ -0,0 +1,22 @@
+"""
+Project Euler Problem 48
+
+Problem:
+
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+"""
+
+import cython
+
+
+@cython.ccall
+@cython.returns(cython.ulonglong)
+def main():
+    ten_ten = 10**10
+    return sum(pow(x, x, ten_ten) for x in range(1, 1001)) % ten_ten
+
+
+if __name__ == '__main__':
+    print(main())
