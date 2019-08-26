9cbc960e123d64b7fabb12f269676a0e19981ebf
Olivia Appleton <olivia.kay.appleton@gmail.com>
2019-08-26 14:04:05 -0700

    Optimizations

--- python/p0048.py
+++ python/p0048.py
@@ -8,11 +8,7 @@ The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
 Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 """
 
-import cython
 
-
-@cython.ccall
-@cython.returns(cython.ulonglong)
 def main():
     ten_ten = 10**10
     return sum(pow(x, x, ten_ten) for x in range(1, 1001)) % ten_ten
