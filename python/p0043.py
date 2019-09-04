c13bd6d449576467dc2db58c34ecdc510383009c
Gabe Appleton <gabe@gabeappleton.me>
2019-09-03 17:48:14 -0700

    Solve 43, 44, 56, 57

---
+++ python/p0043.py
@@ -0,0 +1,41 @@
+"""
+Project Euler Problem 43
+
+Problem:
+
+The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order,
+but it also has a rather interesting sub-string divisibility property.
+
+Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
+
+    d2d3d4=406 is divisible by 2
+    d3d4d5=063 is divisible by 3
+    d4d5d6=635 is divisible by 5
+    d5d6d7=357 is divisible by 7
+    d6d7d8=572 is divisible by 11
+    d7d8d9=728 is divisible by 13
+    d8d9d10=289 is divisible by 17
+
+Find the sum of all 0 to 9 pandigital numbers with this property.
+"""
+from itertools import permutations
+
+from p0008 import groupwise
+from p0055 import from_digits
+
+
+def main():
+    answer = 0
+    divisibility = (1, 2, 3, 5, 7, 11, 13, 17)
+    for d in permutations(range(10), 10):
+        for group, divisor in zip(groupwise(d, 3), divisibility):
+            a, b, c = group
+            if (a * 100 + b * 10 + c) % divisor:
+                break
+        else:
+            print(d)
+            answer += from_digits(d)
+    return answer
+
+if __name__ == '__main__':
+    print(main())
