42e0a487e50cffcd539318cbe0ac7f0dc4c9a8d6
Olivia Appleton <olivia.kay.appleton@gmail.com>
2019-09-06 22:15:28 -0700

    Reduce overhead of test boilerplate

--- c/test_euler.py
+++ c/test_euler.py
@@ -1,3 +1,4 @@
+from functools import partial
 from os import remove
 from platform import processor
 from subprocess import check_call, check_output
@@ -12,7 +13,8 @@ known_slow = {}
 # this is the set of problems where I have the right answer but wrong solution
 
 
-@fixture(params=("{:04}".format(x) for x in sorted(answers.keys())))  # to make sure the benchmarks sort correctly
+# to make sure the benchmarks sort correctly
+@fixture(params=("{:04}".format(x) for x in sorted(answers)))
 def key(request):  # type: ignore
     return request.param
 
@@ -22,17 +24,15 @@ def test_problem(benchmark, key):
     filename = "p{}.c".format(key)
     exename = "p{}.{}".format(key, processor() or 'exe')
     try:
-        check_call(['gcc', '-fms-extensions', '-std=c11', filename, '-o', exename])
-
-        def run_test():
-            return int(check_output(["./{}".format(exename)]).strip())
+        check_call(['gcc', '-Werror', '-std=c11', filename, '-o', exename])
+        run_test = partial(check_output, ["./{}".format(exename)])
 
         if key_i in known_slow:
-            assert answers[key_i] == benchmark.pedantic(
+            assert answers[key_i] == int(benchmark.pedantic(
                 run_test, iterations=1, rounds=1
-            )
+            ).strip())
         else:
-            assert answers[key_i] == benchmark(run_test)
+            assert answers[key_i] == int(benchmark(run_test).strip())
         # sometimes benchmark disables itself, so check for .stats
         if hasattr(benchmark, 'stats') and benchmark.stats.stats.max > 60:
             fail("Exceeding 60s!")
