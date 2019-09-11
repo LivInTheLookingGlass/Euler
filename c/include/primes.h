1af3b9910b0700ccb94b3382904c9b4768b0d354
Gabe Appleton <gabe@gabeappleton.me>
2019-09-10 18:54:16 -0700

    Add documentation to primes.h

--- c/include/primes.h
+++ c/include/primes.h
@@ -28,8 +28,14 @@ static size_t prime_cache_size = 0;
 static size_t prime_cache_idx = 0;
 
 unsigned long long advance_prime_counter(prime_counter *pc) {
+    /**
+     * The function to advance a prime number generator
+     * @pc the counter you want to advance
+     *
+     * Returns the next number in the iteration
+     */
     IterationHead(pc)
-    if (!prime_cache_size)  {
+    if (!prime_cache_size)  {  // if not already done, initialize the prime cache
         prime_cache = (unsigned long long *) malloc(sizeof(unsigned long long) * 4);
         prime_cache[0] = 2;
         prime_cache[1] = 3;
@@ -38,11 +44,11 @@ unsigned long long advance_prime_counter(prime_counter *pc) {
         prime_cache_size = 4;
         prime_cache_idx = 4;
     }
-    if (pc->idx < 2)    {
+    if (pc->idx < 2)    {  // special case this transition so loop is cleaner
         pc->idx = 2;
         return 2;
     }
-    if (pc->idx == 2)    {  // special case this transition so loop is cleaner
+    if (pc->idx == 2)   {  // special case this transition so loop is cleaner
         pc->idx = 3;
         return 3;
     }
@@ -88,6 +94,12 @@ unsigned long long advance_prime_counter(prime_counter *pc) {
 }
 
 prime_counter prime_counter1(unsigned long long stop)  {
+    /**
+     * The base constructor for the prime number generator
+     * @stop: The point where the counter is exhausted
+     *
+     * See prime_counter
+     */
     prime_counter ret;
     IteratorInitHead(ret, advance_prime_counter);
     ret.idx = 0;
@@ -96,10 +108,27 @@ prime_counter prime_counter1(unsigned long long stop)  {
 }
 
 prime_counter prime_counter0()  {
+    /**
+     * The simplest constructor for the prime number generator
+     *
+     * See prime_counter
+     */
     return prime_counter1(-1);
 }
 
 typedef struct {
+    /**
+     * The iterator that allows you to prime factorize a number
+     * @iterator_function: The function to advance the iterator and return the next element
+     * @exhausted: An indicator that tells you if the iterator is exhausted
+     * @started: An indicator that tells you if the interator has moved at all
+     * @phase: An indicator that flips every time the iterator moves
+     * @target: The current target for prime factorization (note: this will change after construction)
+     * @current: The prime number most recently tested
+     * @pc: The prime number generator being used to test
+     *
+     * See IteratorHead
+     */
     IteratorHead
     unsigned long long target;
     unsigned long long current;
@@ -107,7 +136,13 @@ typedef struct {
 } prime_factor_counter;
 
 unsigned long long advance_prime_factor_counter(prime_factor_counter *pfc)  {
-    while (pfc->target != 1 && !pfc->pc.exhausted) {
+    /**
+     * The function to advance a prime factor iterator
+     * @i the counter you want to advance
+     *
+     * Returns the next number in the iteration
+     */
+    while (pfc->target != 0 && pfc->target != 1 && !pfc->pc.exhausted) {
         pfc->current = (unsigned long long) next(pfc->pc);
         if (pfc->target % pfc->current == 0)    {
             pfc->target /= pfc->current;
@@ -115,10 +150,19 @@ unsigned long long advance_prime_factor_counter(prime_factor_counter *pfc)  {
             return pfc->current;
         }
     }
+    pfc->exhausted = true;
     return -1;
 }
 
 prime_factor_counter prime_factors(unsigned long long n)    {
+    /**
+     * The base constructor for the prime factors iterator
+     * @n: The non-zero number you wish to factor
+     *
+     * WARNING: if you put in 0, behaviour is undefined
+     *
+     * See prime_factor_counter
+     */
     prime_factor_counter ret;
     IteratorInitHead(ret, advance_prime_factor_counter);
     ret.current = 0;
