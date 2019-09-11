10816b62d5b1e4f50b5af6b70ab19fc25d35ff9e
Olivia Appleton <olivia.kay.appleton@gmail.com>
2019-09-10 22:23:39 -0700

    Add slightly more optimization to 76

--- c/p0076.c
+++ c/p0076.c
@@ -4,6 +4,10 @@ Project Euler Problem 76
 I ended up having to do this iteratively, which I'm not super happy with. I feel like there is almost certainly a
 closed-form solution to this, but I was unable to figure it out.
 
+Revision 1:
+
+Have counts store values instead of numeral counts to shave a few seconds off
+
 Problem:
 
 It is possible to write five as a sum in exactly six different ways:
@@ -28,17 +32,18 @@ int main(int argc, char const *argv[])  {
     while (!counts[100]) {
         sum = counts[1];
         for (i = 2; i < 100; i++) {
-            sum += counts[i] * i;
+            sum += counts[i];
         }
         counts[1]++;
         if (sum > 100)  {
             idx = 1;
             do {
                 counts[idx] = 0;
-                counts[++idx]++;
+                idx++;
+                counts[idx] += idx;
                 sum = counts[1];
                 for (i = 2; i < 100; i++) {
-                    sum += counts[i] * i;
+                    sum += counts[i];
                 }
             } while (sum > 100);
             counts[1] = 100 - sum;
