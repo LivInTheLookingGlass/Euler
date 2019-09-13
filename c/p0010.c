1c9af912b4071f991551531893d60567665b40e9
Olivia Appleton <olivia.kay.appleton@gmail.com>
2019-09-12 20:56:16 -0700

    Add prime number sieve, solve 10 (TODO, have prime_counter pull from prime_sieve)

---
+++ c/p0010.c
@@ -0,0 +1,24 @@
+/*
+Project Euler Problem 10
+
+This is where my prime infrastructure shows its weak points, since the C version doesn't have a sieve.
+
+Problem:
+
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+*/
+#include <stdio.h>
+#include "include/primes.h"
+
+
+int main(int argc, char const *argv[])  {
+    unsigned long long tmp, answer = 0;
+    prime_sieve ps = prime_sieve0();
+    while ((tmp = next(ps)) < 2000000)  {
+        answer += tmp;
+    }
+    printf("%llu", answer);  // this is because of a bug
+    return 0;
+}
