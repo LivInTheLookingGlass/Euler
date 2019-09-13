6ef9eb2c1a6d20176cd35ebcbcb7f397931a1534
Olivia Appleton <olivia.kay.appleton@gmail.com>
2019-09-12 20:57:53 -0700

    Correct p0001 formatting/documentation

--- c/p0001.c
+++ c/p0001.c
@@ -1,9 +1,6 @@
 /*
 Project Euler Problem 1
 
-I decided I wanted to take a funcitonal approach on this one. It also uses only
-lazy functions, so it should take minimal memory usage.
-
 I know that this could be done faster with a traditional for loop, but I wanted
 to see if iterators were reasonably possible in C, since it makes the prime
 number infrastructure a lot easier to set up.
@@ -19,8 +16,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 #include <stdio.h>
 #include "include/iterator.h"
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[])  {
     unsigned long long answer = 0;
     counter c = counter3(0, 1000, 3);
     while (!c.exhausted)    {
@@ -28,11 +24,11 @@ int main(int argc, char const *argv[])
     }
     c = counter3(0, 1000, 5);
     while (!c.exhausted)    {
-        answer += (unsigned long long) next(c);
+        answer += next(c);
     }
     c = counter3(0, 1000, 15);
     while (!c.exhausted)    {
-        answer -= (unsigned long long) next(c);
+        answer -= next(c);
     }
     printf("%llu\n", answer);
     return 0;
