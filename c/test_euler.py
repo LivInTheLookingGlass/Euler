93bacf65a8fdca52ed3134656c1a94f4b3767491
Olivia Appleton <olivia.kay.appleton@gmail.com>
2019-09-16 14:02:54 -0700

    Add/refactor lots of system context

--- c/test_euler.py
+++ c/test_euler.py
@@ -1,8 +1,9 @@
+from atexit import register
 from functools import partial
-from os import remove, sep
+from os import makedirs, remove, sep
 from os.path import expanduser
 from platform import processor, machine, system
-from shutil import which
+from shutil import rmtree, which
 from subprocess import check_call, check_output
 
 from pytest import fail, fixture, skip
@@ -37,57 +38,75 @@ elif IN_WINDOWS:
         EXE_EXT += "_64"
 elif IN_TERMUX:
     # processor() doesn't seem to work on Termux
-    EXE_EXT = check_output('lscpu').split()[1]
+    EXE_EXT = check_output('lscpu').split()[1].decode()
 else:
     EXE_EXT = "exe"
 
+SOURCE_TEMPLATE = "p{:04}.c"
 if IN_TERMUX:
-    EXE_TEMPLATE = "{}/p{{}}.{}".format(expanduser("~"), EXE_EXT)
+    EXE_TEMPLATE = "{}/p{{:04}}.{{}}.{}".format(expanduser("~"), EXE_EXT)
     # Termux can't make executable files outside of $HOME
 else:
-    EXE_TEMPLATE = ".{}p{{}}.{}".format(sep, EXE_EXT)
+    EXE_TEMPLATE = ".{}p{{:04}}.{{}}.{}".format(sep, EXE_EXT)
     # include sep in the recipe so that Windows won't complain
 
 # compiler variables section
-USING_CL = False
-USING_GCC = False
-USING_CLANG = False
+compilers = []
+templates = {
+    'GCC': "gcc {} -O2 -lm -Werror -std=c11 -o {}",
+    'CLANG': "clang {} -O2 -lm -Werror -std=c11 -o {}",
+    'CL': "cl -Fe:{1} -Foobjs\\ -O2 -TC {0}",
+}
 
-if which('gcc'):
-    COMPILER_TEMPLATE = "gcc {} -O -lm -Werror -std=c11 -o {}"
-    USING_GCC = True
-elif which('clang'):
-    COMPILER_TEMPLATE = "clang {} -O -lm -Werror -std=c11 -o {}"
-    USING_CLANG = True
-elif IN_WINDOWS and which('cl'):
-    COMPILER_TEMPLATE = "cl /Fe:{1} /O2 /TC {0}"
-    USING_CL = True
-else:
-    raise RuntimeError("Unsupported system!")
+if not IN_TERMUX and which('gcc'):  # Termux maps gcc->clang
+    compilers.append('GCC')
+if which('clang'):
+    compilers.append('CLANG')
+if which('cl'):
+    @register
+    def cleanup_objs():
+        try:
+            rmtree('objs')  # only present with cl compiler
+        except Exception:
+            pass  # if in multiprocess, this race-conditions
+
+    makedirs('objs', exist_ok=True)
+    compilers.append('CL')
+if which('pcc'):
+    raise NotImplementedError()
+if which('tcc'):
+    raise NotImplementedError()
+if which('icc'):
+    raise NotImplementedError()
+if not compilers:
+    raise RuntimeError("No compilers detected!")
 
 
-# to make sure the benchmarks sort correctly
-@fixture(params=("{:04}".format(x) for x in sorted(answers)))
-def key(request):  # type: ignore
+@fixture(params=compilers)
+def compiler(request):  # type: ignore
     return request.param
 
 
-def test_problem(benchmark, key):
-    key_i = int(key)
-    filename = "p{}.c".format(key)
-    exename = EXE_TEMPLATE.format(key)
+# to make sure the benchmarks sort correctly
+@fixture(params=("{:03}".format(x) for x in sorted(answers)))
+def key(request):  # type: ignore
+    return int(request.param)  # reduce casting burden on test
+
+
+def test_problem(benchmark, key, compiler):
+    if IN_TERMUX and key in known_slow:
+        skip()
+    filename = SOURCE_TEMPLATE.format(key)
+    exename = EXE_TEMPLATE.format(key, compiler)  # need to have both to keep name unique
     try:
-        check_call(COMPILER_TEMPLATE.format(filename, exename).split())
+        check_call(templates[compiler].format(filename, exename).split())
         run_test = partial(check_output, [exename])
 
-        if key_i in known_slow:
-            if IN_TERMUX:
-                skip()
-            assert answers[key_i] == int(benchmark.pedantic(
-                run_test, iterations=1, rounds=1
-            ).strip())
+        if key in known_slow:
+            answer = benchmark.pedantic(run_test, iterations=1, rounds=1)
         else:
-            assert answers[key_i] == int(benchmark(run_test).strip())
+            answer = benchmark(run_test)
+        assert answers[key] == int(answer.strip())
         # sometimes benchmark disables itself, so check for .stats
         if hasattr(benchmark, 'stats') and benchmark.stats.stats.max > 60:
             fail("Exceeding 60s!")
@@ -96,8 +115,3 @@ def test_problem(benchmark, key):
             remove(exename)
         except Exception:
             pass  # might not have been made yet
-        if USING_CL:
-            try:
-                remove("p{}.obj".format(key))  # only present with cl compiler
-            except Exception:
-                pass  # might not have been made yet
