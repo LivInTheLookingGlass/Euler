ac9659d351d97725e17c1f5b273958de0f0489cb
Olivia Appleton <olivia.kay.appleton@gmail.com>
2019-09-17 14:24:51 -0700

    Make p0002.py more efficient

--- python/p0002.py
+++ python/p0002.py
@@ -36,14 +36,38 @@ def fib() -> Iterator[int]:
         a, b = b, a + b
 
 
+def fib_by_3(start_index: int = 0) -> Iterator[int]:
+    """This generator goes through the fibonacci sequence skipping by 3s. This works because:
+    F[n] = F[n-1]          + F[n-2]
+    F[n] = F[n-2] + F[n-3] + F[n-2]
+    F[n] = 2 * F[n-2]            + F[n-3]
+    F[n] = 2 * (F[n-3] + F[n-4]) + F[n-3]
+    F[n] = 3 * F[n-3] + 2 * F[n-4]
+    F[n] = 3 * F[n-3] + F[n-4] + F[n-5] + F[n-6]
+    F[n] = 4 * F[n-3]                   + F[n-6]
+    """
+    orig = fib()
+    a = 0
+    if start_index:
+        for _ in range(start_index - 1):
+            next(orig)
+        a = next(orig)
+    next(orig)  # start + 1
+    next(orig)  # start + 2
+    b = next(orig)  # start + 3
+    del orig
+    yield a
+    while True:
+        yield b
+        a, b = b, a + b * 4
+
+
 def main():
-    f = fib()
+    f = fib_by_3()  # this works because every third value is even
     answer = val = 0
     while val < 4_000_000:
         answer += val
-        next(f)  # odd
-        next(f)  # odd
-        val = next(f)  # even
+        val = next(f)
     return answer
 
 
