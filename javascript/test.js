37af1d7146d25b0da8e913018f16d8b5d1997914
Gabe Appleton <gabe@gabeappleton.me>
2019-09-17 14:25:04 -0700

    Initial work on js benchmark reporting

--- javascript/test.js
+++ javascript/test.js
@@ -5,6 +5,7 @@ const answers = {
     1: 233168
 };
 const known_slow = [];
+let benchmark_report = '';
 
 for (question in answers)   {
     const formatted_question = `${question}`.padStart(4, '0');
@@ -22,8 +23,22 @@ for (question in answers)   {
             this.slow(300000); // five minutes
             var b = benchmark.Benchmark(formatted_question, module.main, {'minSamples': 10});
             var [results] = benchmark.Benchmark.invoke([b], 'run');
-            assert.ok(60 > Math.max(...(results.stats.sample)));
+            const max = Math.max(...(results.stats.sample));
+            assert.ok(60 > max);
+            benchmark_report += `================= p${formatted_question} =================\n`;
+            benchmark_report += `Maximum time:            ${max.toFixed(6)}s\n`;
+            benchmark_report += `Minimum time:            ${Math.min(...(results.stats.sample)).toFixed(6)}s\n`;
+            benchmark_report += `Average time:            ${results.stats.mean.toFixed(6)}s\n`;
+            benchmark_report += `OPS:                     ${results.hz.toFixed(6)}/s\n`;
+            benchmark_report += `Standard Deviation:      ${results.stats.deviation.toFixed(6)}s\n`;
+            benchmark_report += `Margin of Error:         ${results.stats.moe.toFixed(6)}s\n`;
+            benchmark_report += `Iterations:              ${results.stats.sample.length}\n\n\n`;
             done();
+            process.stdout.write(`        Max time: ${max.toFixed(6)}s\n`);
         });
     });
 }
+
+after(()=>{
+    process.stdout.write(benchmark_report);
+});
