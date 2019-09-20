07e2625df70f10c2e260c2f5032408e60ff1de7b
Olivia Appleton <olivia.kay.appleton@gmail.com>
2019-09-19 20:52:14 -0700

    Have javascript conform to templating system

--- javascript/p0001.js
+++ javascript/p0001.js
@@ -1,3 +1,16 @@
+/*
+Project Euler Problem 1
+
+Did this the old fashioned way, because this was before I figured out the closed form solution
+
+Problem:
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
 exports.main = function()   {
     let answer = 0;
     for (let i = 3; i < 1000; i += 3)    {
