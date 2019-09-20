4e53a041a41cc6b0481360e9e271fd19f70bded8
Gabe Appleton <gabe@gabeappleton.me>
2019-09-20 16:26:44 -0700

    Attempt fix to pypy pip

--- .scripts/travis_script.sh
+++ .scripts/travis_script.sh
@@ -7,10 +7,9 @@ if [ $pyver ]; then
         get_python_environment  $pydist $pyver;
     fi
     if [ $pyver == pypy3 ]; then
-        brew install $pyver; export PYTHON_EXE=$pyver;
-        curl $GET_PIP_URL > $DOWNLOADS_SDIR/get-pip.py;
-        sudo $PYTHON_EXE $DOWNLOADS_SDIR/get-pip.py --ignore-installed;
-        export PIP_CMD="sudo $PYTHON_EXE -m pip";
+        brew install $pyver;
+        export PYTHON_EXE=$pyver;
+        export PIP_CMD="$PYTHON_EXE -m pip";
     fi
     $PIP_CMD install virtualenv;
     virtualenv -p $PYTHON_EXE venv;
