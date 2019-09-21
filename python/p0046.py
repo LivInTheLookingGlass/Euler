bfaf83b131b55a6f39f2735a73922a9eee40525e
Gabe Appleton <gabe@gabeappleton.me>
2019-09-20 17:04:12 -0700

    Significant speedup to python/p0046

--- python/p0046.py
+++ python/p0046.py
@@ -1,6 +1,12 @@
 """
 Project Euler Problem 46
 
+Didn't even need to rearrange the formula to solve it trivially
+
+Revision 1:
+
+It turns out that rearranging the formula let me cut the time by ~50x
+
 Problem:
 
 It was proposed by Christian Goldbach that every odd composite number can be written as the sum of a prime and twice a
@@ -17,19 +23,21 @@ It turns out that the conjecture was false.
 
 What is the smallest odd composite that cannot be written as the sum of a prime and twice a square?
 """
-from itertools import count
+from itertools import count, takewhile
+from math import ceil, sqrt
 
 from p0003 import primes
 from p0007 import is_prime
 
 
 def main():
+    cached_primes = tuple(primes(6000))
     for goal in count(35, 2):
         if is_prime(goal):
             continue
-        for p in primes(goal):
+        for p in takewhile(goal.__gt__, cached_primes):
             done = False
-            for x in range(1, (goal - p) // 2):
+            for x in range(1, ceil(sqrt((goal - p) / 2)) + 1):
                 if p + 2 * x * x == goal:
                     done = True
                     break
