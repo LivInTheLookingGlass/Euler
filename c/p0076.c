bbed032d71df4316dcf3eae91d8f60369368eccd
Gabe Appleton <gabe@gabeappleton.me>
2019-09-23 17:32:18 -0700

    Manually expand a cumbersome loop

--- c/p0076.c
+++ c/p0076.c
@@ -8,6 +8,11 @@ Revision 1:
 
 Have counts store values instead of numeral counts to shave a few seconds off
 
+Revision 2:
+
+Manually expand the sum loop in the preprocessor to try and get TCC output to be faster. Shaved a ~1/3 of runtime in
+both CL and GCC in my initial testing.
+
 Problem:
 
 It is possible to write five as a sum in exactly six different ways:
@@ -23,17 +28,14 @@ How many different ways can one hundred be written as a sum of at least two
 positive integers?
 */
 #include <stdio.h>
+#include "include/macros.h"
 
 
 int main(int argc, char const *argv[])  {
     unsigned int answer = 0;
-    unsigned char idx, i, sum, counts[101] = {0};
+    unsigned char idx, i, sum = 100, counts[101] = {0};
     counts[1] = 100;
     while (!counts[100]) {
-        sum = counts[1];
-        for (i = 2; i < 100; ++i) {
-            sum += counts[i];
-        }
         ++counts[1];
         if (sum > 100)  {
             idx = 1;
@@ -42,15 +44,17 @@ int main(int argc, char const *argv[])  {
                 ++idx;
                 counts[idx] += idx;
                 sum = counts[1];
-                for (i = 2; i < 100; ++i) {
-                    sum += counts[i];
-                }
+                i = 1;
+                REPEAT98(sum += counts[++i]);
             } while (sum > 100);
             counts[1] = 100 - sum;
         }
         else if (sum == 100)    {
             ++answer;
         }
+        sum = counts[1];
+        i = 1;
+        REPEAT98(sum += counts[++i]);
     }
     printf("%u", answer);
     return 0;
