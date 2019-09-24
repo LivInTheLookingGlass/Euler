5c209d081ddf9d365c5a9cad1bfc0e00580c4353
Olivia Appleton <olivia.kay.appleton@gmail.com>
2019-09-23 17:32:18 -0700

    Manually expand a cumbersome loop

--- c/include/macros.h
+++ c/include/macros.h
@@ -1,7 +1,19 @@
+#ifndef _MACROS_H
+#define _MACROS_H
+
 #ifndef max
     #define max(a, b) (((a) > (b)) ? (a) : (b))
 #endif
 
 #ifndef min
     #define min(a, b) (((a) < (b)) ? (a) : (b))
+#endif
+
+#define REPEAT2(x) x; x
+#define REPEAT4(x) REPEAT2(REPEAT2(x))
+#define REPEAT16(x) REPEAT4(REPEAT4(x))
+#define REPEAT32(x) REPEAT2(REPEAT16(x))
+#define REPEAT64(x) REPEAT2(REPEAT16(x))
+#define REPEAT98(x) REPEAT64(x); REPEAT32(x); REPEAT2(x)
+
 #endif
