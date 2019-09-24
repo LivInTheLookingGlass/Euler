76828fa546968be674b80dcef431509ea56356b3
Gabe Appleton <gabe@gabeappleton.me>
2019-09-23 22:34:13 -0700

    Fix c/p0076

--- c/p0076.c
+++ c/p0076.c
@@ -13,6 +13,12 @@ Revision 2:
 Manually expand the sum loop in the preprocessor to try and get TCC output to be faster. Shaved a ~1/3 of runtime in
 both CL and GCC in my initial testing.
 
+Revision 3:
+
+After testing on non-Windows systems, I found that Revision 2 royally borked it up. I reverted this, then applied an
+optimization I found earlier today. The number of solutions to a + 2b + n = 100, where a, b, n in [0, 100] is
+(100-n)/2. This brought runtime on TCC from ~3min -> ~1min and clang from ~6s -> ~2s. 
+
 Problem:
 
 It is possible to write five as a sum in exactly six different ways:
@@ -33,28 +39,26 @@ positive integers?
 
 int main(int argc, char const *argv[])  {
     unsigned int answer = 0;
-    unsigned char idx, i, sum = 100, counts[101] = {0};
-    counts[1] = 100;
+    unsigned char idx, i, sum = 100, counts[101] = {0, 0, 100, 0};
     while (!counts[100]) {
-        ++counts[1];
-        if (sum > 100)  {
-            idx = 1;
+        counts[2] += 2;
+        if (sum >= 100) {
+            answer += (100 + counts[2] - sum) / 2;
+            idx = 2;
             do {
                 counts[idx] = 0;
                 ++idx;
                 counts[idx] += idx;
-                sum = counts[1];
-                i = 1;
-                REPEAT98(sum += counts[++i]);
+                sum = counts[2];
+                for (i = 3; i < 100; ++i) {
+                    sum += counts[i];
+                }
             } while (sum > 100);
-            counts[1] = 100 - sum;
         }
-        else if (sum == 100)    {
-            ++answer;
+        sum = counts[2];
+        for (i = 3; i < 100; ++i) {
+            sum += counts[i];
         }
-        sum = counts[1];
-        i = 1;
-        REPEAT98(sum += counts[++i]);
     }
     printf("%u", answer);
     return 0;
