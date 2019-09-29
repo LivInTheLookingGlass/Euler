d4b421f1e4ba5b40e7975642f5249123b54eb550
Gabe Appleton <gabe@gabeappleton.me>
2019-09-29 12:15:24 -0700

    First attempt at PCC build for travis

--- .scripts/travis_script.sh
+++ .scripts/travis_script.sh
@@ -19,12 +19,35 @@ if [ $pyver ]; then
     else
         make pytest PY=python LINT=false MYPY= USER_FLAG=
     fi
-else
+elif [ $jsver ]; then
     if [ $linter ]; then
         cd javascript;
         npm install eslint-config-google@latest eslint@>=5.16.0;
         npx eslint *.js && echo "Linting successful!"
     else
         make jstest;
+    fi
+else
+    if [ $linter ]; then
+        brew install llvm;
+        ln -s "/usr/local/opt/llvm/bin/clang-tidy" "/usr/local/bin/clang-tidy";
+        make clint;
+    else
+        if [ $pcc ]; then
+            brew install flex bison
+            mkdir pcc pcc-libs
+            wget -O - -o /dev/null http://pcc.ludd.ltu.se/ftp/pub/pcc-releases/pcc-1.1.0.tgz | tar -xz --no-seek -C pcc --strip-components=1
+            wget -O - -o /dev/null http://pcc.ludd.ltu.se/ftp/pub/pcc-releases/pcc-libs-1.1.0.tgz | tar -xz --no-seek -C pcc-libs --strip-components=1
+            cd pcc
+            ./configure
+            sudo make && make install
+            cd ../pcc-libs
+            ./configure
+            sudo make && make install
+            cd ..
+        else
+            brew install $COMPILER_OVERRIDE;
+        fi
+        make ctest;
     fi
 fi
