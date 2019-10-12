16dea4869ef18c4298696a8e2176f60f1f3f655e
Olivia Appleton <olivia.kay.appleton@gmail.com>
2019-10-11 18:17:38 -0700

    Add 73 to known_slow (it's marginal)

--- python/test_euler.py
+++ python/test_euler.py
@@ -89,7 +89,7 @@ answers = {
     206: 1389019170,
 }
 
-known_slow = {76, 118, 145}
+known_slow = {73, 76, 118, 145}
 # this is the set of problems where I have the right answer but wrong solution
 
 IN_TERMUX = bool(which('termux-setup-storage'))
