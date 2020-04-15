c8ff26a195aa66c69becb204f039791391796f69
Olivia Appleton <olivia.kay.appleton@gmail.com>
2020-04-14 20:12:37 -0400

    Style guide fixes

--- javascript/p0000_template.js
+++ javascript/p0000_template.js
@@ -1,10 +1,10 @@
 /**
 * Project Euler Template
-* 
+*
 * This template is used to format Project Euler solution scripts. This paragraph
 * should be replaced by a description of how I approached the problem, as well as
 * critique.
-* 
+*
 * This paragraph should be replaced by the problem description, excluding images.
 */
 
