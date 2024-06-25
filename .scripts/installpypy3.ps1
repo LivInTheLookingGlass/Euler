97c19d4a8fcaeaf86479a29dba2c1aa385362778
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-06-25 00:13:05 -0500

    Move more scripted workflows to GitHub Actions

--- .scripts/installpypy3.ps1
+++
@@ -1,4 +0,0 @@
-python -c "import urllib; urllib.urlretrieve('https://bitbucket.org/pypy/pypy/downloads/pypy3.6-v7.1.1-win32.zip', 'pypy3.6-v7.1.1-win32.zip'); urllib.urlretrieve('https://bootstrap.pypa.io/get-pip.py', 'get-pip.py')"
-7z x pypy3.6-v7.1.1-win32.zip > Out-Null
-pypy3.6-v7.1.1-win32\pypy get-pip.py
-pypy3.6-v7.1.1-win32\pypy -m pip install setuptools
