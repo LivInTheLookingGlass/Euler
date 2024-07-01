9435d43470b593cac1cda532981293d76424b4bc
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-07-01 03:24:19 -0500

    Make linter happy

--- javascript/p0001.js
+++ javascript/p0001.js
@@ -1,13 +1,13 @@
 /**
 * Project Euler Problem 1
-* 
+*
 * Did this the old fashioned way, because this was before I figured out the closed form solution
-* 
+*
 * Problem:
-* 
+*
 * If we list all the natural numbers below 10 that are multiples of 3 or 5, we
 * get 3, 5, 6 and 9. The sum of these multiples is 23.
-* 
+*
 * Find the sum of all the multiples of 3 or 5 below 1000.
 */
 exports.p0001 = function() {
