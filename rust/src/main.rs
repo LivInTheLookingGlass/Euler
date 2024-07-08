125042b9403023312499e72dd0ef6d64ae812796
Olivia Appleton <liv@OliviaAppleton.com>
2024-07-07 19:09:04 -0500

    Fix type error, include modules dynamically

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,16 +1,16 @@
 #[cfg(test)]
 use std::time::Duration;
 
-#[cfg(test)]
 use seq_macro::seq;
 #[cfg(test)]
 use rstest::rstest;
 
-mod p0001;
-mod p0002;
+seq!(N in 0001..=0002 {
+mod p~N;
+});
 
 type ProblemType = fn() -> u64;
-type ProblemRef = (&str, ProblemType, u64);
+type ProblemRef<'a> = (&'a str, ProblemType, u64);
 const ANSWERS: [ProblemRef; 2] = [
     ("p0001", p0001::p0001, 233168),
     ("p0002", p0002::p0002, 4613732)
