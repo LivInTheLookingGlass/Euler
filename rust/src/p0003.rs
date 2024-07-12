ac5f40c82b9bd9920e805df79f60952bbeffa7b7
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-07-11 23:23:21 -0500

    Fix typing

--- rust/src/p0003.rs
+++ rust/src/p0003.rs
@@ -12,5 +12,5 @@ What is the largest prime factor of the number 600851475143 ?
 use crate::primes::prime_factors;
 
 pub fn p0003() -> u64 {
-    return prime_factors(600851475143).max();
+    return prime_factors(600851475143).max()?;
 }
