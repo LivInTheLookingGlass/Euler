33d3ddfb187e560ab9f532e11a49410156101012
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-07-11 23:20:42 -0500

    More test versions, fix import issue

--- rust/src/p0003.rs
+++ rust/src/p0003.rs
@@ -9,8 +9,8 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 */
-mod primes;
+use crate::primes::prime_factors;
 
 pub fn p0003() -> u64 {
-    return primes::prime_factors(600851475143).max();
+    return prime_factors(600851475143).max();
 }
