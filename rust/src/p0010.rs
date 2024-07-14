cf1e23e5223acb2be4e455abd2fc5414756197e0
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-07-14 14:39:12 -0500

    solve p0004 in rust, update documentation

--- rust/src/p0010.rs
+++ rust/src/p0010.rs
@@ -1,3 +1,15 @@
+/*
+Project Euler Problem 10
+
+This one was also relatively easy, especially given the work I have
+done on my prime number infrastructure.
+
+Problem:
+
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+*/
 use crate::primes::primes;
 
 pub fn p0010() -> u64 {
