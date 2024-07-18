4ac584ef7c9840a6d2a2f3cec9b25b4ba98871e6
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-07-17 22:46:12 -0500

    Add docs for new rust solutions

--- rust/src/p0005.rs
+++ rust/src/p0005.rs
@@ -1,3 +1,18 @@
+/*
+Project Euler Problem 5
+
+I solved this problem by testing all combinations of the various multiples. I
+actually tried to solve this by hand before doing this, and it wasn't terribly
+hard. The answer turns out to be (2**4 * 3**2 * 5 * 7 * 11 * 13 * 17 * 19)
+
+Problem:
+
+2520 is the smallest number that can be divided by each of the numbers from 1
+to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the
+numbers from 1 to 20?
+*/
 use itertools::Itertools;
 
 pub fn p0005() -> u64 {
