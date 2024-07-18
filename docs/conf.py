58cbac3b387ea9fe1cdc339e22e3095b9ed3b2b7
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-07-18 00:58:43 -0500

    Let's try that relocation again^2

--- docs/conf.py
+++ docs/conf.py
@@ -104,7 +104,7 @@ def setup(app):
         sizes = [lang[1] for lang in langs]
         fig, ax = plt.subplots()
         pie = ax.pie(sizes, labels=labels, autopct='%1.1f%%', labeldistance=None, pctdistance=0.85)
-        plt.legend(title='Languages', loc='right')
+        plt.legend(title='Languages', loc='right', bbox_to_anchor=(1,0.5))
         plt.savefig('languages.svg', transparent=True, bbox_inches='tight')
 
     app.add_domain(custom_domain(
