7c31f1f5c824e17de8161b67f8e9bd768043a7fe
Olivia Appleton <liv@OliviaAppleton.com>
2024-07-18 23:59:45 -0500

    Solve problems 6, 8, 9 in C#

---
+++ csharp/Euler/p0006.cs
@@ -0,0 +1,39 @@
+/*
+Project Euler Problem 6
+
+This turned out to be really easy
+
+Problem:
+
+The sum of the squares of the first ten natural numbers is,
+1**2 + 2**2 + ... + 10**2 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)**2 = 55**2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural
+numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred
+natural numbers and the square of the sum.
+*/
+using System;
+
+namespace Euler
+{
+    public class p0006 : IEuler
+    {
+        public Task<Int64> Answer()
+        {
+            Int64 sum_of_squares = 0,
+                  sum = 0;
+            for (int i = 1; i < 101; i++)
+            {
+                sum += i;
+                sum_of_squares += i * i;
+            }
+            Int64 square_of_sum = sum * sum;
+            return Task.FromResult<Int64>(square_of_sum - sum_of_squares);
+        }
+    }
+}
---
+++ csharp/Euler/p0009.cs
@@ -0,0 +1,40 @@
+/*
+Project Euler Problem 9
+
+
+
+Problem:
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a**2 + b**2 = c**2
+
+For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+*/
+using System;
+
+namespace Euler
+{
+    public class p0009 : IEuler
+    {
+        private const int MAX = 500;
+        public Task<Int64> Answer()
+        {
+            for (int c = 3; c < MAX; c++) {
+                int c_square = c * c;
+                for (int b = 2; b < c; b++) {
+                    int b_square = b * b;
+                    for (int a = 1; a < b; a++) {
+                        int a_square = a * a;
+                        if (a_square + b_square == c_square && a + b + c == 1000) {
+                            return Task.FromResult<Int64>(a * b * c);
+                        }
+                    }
+                }
+            }
+            return Task.FromResult<Int64>(0);
+        }
+    }
+}
