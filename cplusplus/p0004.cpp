1b95525a5eba40773c556987082052faed1e7665
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-07-22 12:21:31 -0500

    continue aiming for C++98 I will give up if this takes too long and go to C++11

--- cplusplus/p0004.cpp
+++ cplusplus/p0004.cpp
@@ -13,6 +13,7 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 #ifndef EULER_P0004
 #define EULER_P0004
 #include <algorithm>
+#include <cstdio>
 #include <iostream>
 
 unsigned int p0004() {
