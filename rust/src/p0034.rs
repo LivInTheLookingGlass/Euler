5fd4f626a2fa032244eee95c20d4d5cc56e7c6b9
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-07-23 01:46:19 -0500

    Oh good, Python's b-strings work

--- rust/src/p0034.rs
+++ rust/src/p0034.rs
@@ -21,7 +21,7 @@ pub fn p0034() -> i128 {
     for x in 10..100000 {
         let sum = x.to_string()
                    .bytes()
-                   .fold(0, |a, b| a + factorial::<u32>(b - u8::from('0')));
+                   .fold(0, |a, b| a + factorial::<u32>(b - b'0'));
         if sum == x {
             answer += x;
         }
