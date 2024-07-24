825c7d7252c5c807467f0b222ab44734d6f55e53
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-07-24 15:24:53 -0500

    Fix typing errors

--- rust/src/p0357.rs
+++ rust/src/p0357.rs
@@ -22,8 +22,8 @@ pub fn p0357() -> i128 {
     );
     for n in (6..100000000).step_by(4) {
         // n can't be odd (unless 1) because then n + n/d is even, and can't be a multiple of 4 as shown below
-        broken = false;
-        for d in proper_divisors(n).chain(vec![n].into_iter()) {
+        let broken = false;
+        for d in proper_divisors(n).into_iter().chain(vec![n].into_iter()) {
             if prime_squares.contains(d) || !is_prime(d + n / d) {
                 // this works because if n=kp^2, then whenever d=p, (p + kp^2/p) = (k+1)p, which isn't prime
                 // but since detecting if n % d^2 == 0 is expensive, I just wait for p^2 to show up
