19193b36d70fe69109724f2913fd0bc932ac832c
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-07-30 17:39:07 -0500

    make timeout failures more explicit

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,9 @@
 #[cfg(test)]
-use std::time::Duration;
+use std::env::var_os;
 #[cfg(test)]
 use std::iter::zip;
+#[cfg(test)]
+use std::time::{Duration,Instant};
 
 use seq_macro::seq;
 #[cfg(test)]
@@ -28,36 +30,36 @@ mod p0357;
 mod p0836;
 
 type ProblemType = fn() -> i128;
-type ProblemRef<'a> = (&'a u32, ProblemType, i128);
+type ProblemRef<'a> = (&'a u32, ProblemType, bool, i128);
 
 fn get_problem<'b>(n: u32) -> ProblemRef<'b> {
     return match n {
-        1 =>   (  &1, p0001::p0001, 233168),
-        2 =>   (  &2, p0002::p0002, 4613732),
-        3 =>   (  &3, p0003::p0003, 6857),
-        4 =>   (  &4, p0004::p0004, 906609),
-        5 =>   (  &5, p0005::p0005, 232792560),
-        6 =>   (  &6, p0006::p0006, 25164150),
-        7 =>   (  &7, p0007::p0007, 104743),
-        8 =>   (  &8, p0008::p0008, 23514624000),
-        9 =>   (  &9, p0009::p0009, 31875000),
-        10 =>  ( &10, p0010::p0010, 142913828922),
-        11 =>  ( &11, p0011::p0011, 70600674),
-        12 =>  ( &12, p0012::p0012, 76576500),
-        13 =>  ( &13, p0013::p0013, 5537376230),
-        14 =>  ( &14, p0014::p0014, 837799),
-        15 =>  ( &15, p0015::p0015, 137846528820),
-        17 =>  ( &17, p0017::p0017, 21124),
-        18 =>  ( &18, p0018::p0018, 1074),
-        22 =>  ( &22, p0022::p0022, 871198282),
-        24 =>  ( &24, p0024::p0024, 2783915460),
-        34 =>  ( &34, p0034::p0034, 40730),
-        69 =>  ( &69, p0069::p0069, 510510),
-        76 =>  ( &76, p0076::p0076, 190569291),
-        77 =>  ( &77, p0077::p0077, 71),
-        87 =>  ( &87, p0087::p0087, 1097343),
-        357 => (&357, p0357::p0357, 1739023853137),
-        836 => (&836, p0836::p0836, i128::from_ne_bytes(*b"\x00\x00aprilfoolsjoke")),
+        1 =>   (  &1, p0001::p0001, false, 233168),
+        2 =>   (  &2, p0002::p0002, false, 4613732),
+        3 =>   (  &3, p0003::p0003, false, 6857),
+        4 =>   (  &4, p0004::p0004, false, 906609),
+        5 =>   (  &5, p0005::p0005, false, 232792560),
+        6 =>   (  &6, p0006::p0006, false, 25164150),
+        7 =>   (  &7, p0007::p0007, false, 104743),
+        8 =>   (  &8, p0008::p0008, false, 23514624000),
+        9 =>   (  &9, p0009::p0009, false, 31875000),
+        10 =>  ( &10, p0010::p0010, false, 142913828922),
+        11 =>  ( &11, p0011::p0011, false, 70600674),
+        12 =>  ( &12, p0012::p0012, false, 76576500),
+        13 =>  ( &13, p0013::p0013, false, 5537376230),
+        14 =>  ( &14, p0014::p0014, false, 837799),
+        15 =>  ( &15, p0015::p0015, false, 137846528820),
+        17 =>  ( &17, p0017::p0017, false, 21124),
+        18 =>  ( &18, p0018::p0018, false, 1074),
+        22 =>  ( &22, p0022::p0022, false, 871198282),
+        24 =>  ( &24, p0024::p0024, false, 2783915460),
+        34 =>  ( &34, p0034::p0034, false, 40730),
+        69 =>  ( &69, p0069::p0069, false, 510510),
+        76 =>  ( &76, p0076::p0076, false, 190569291),
+        77 =>  ( &77, p0077::p0077, false, 71),
+        87 =>  ( &87, p0087::p0087, false, 1097343),
+        357 => (&357, p0357::p0357, true, 1739023853137),
+        836 => (&836, p0836::p0836, false, i128::from_ne_bytes(*b"\x00\x00aprilfoolsjoke")),
         _ => panic!(),
     };
 }
@@ -81,7 +83,7 @@ fn main() {
     answers.push(357);
 
     for id  in answers {
-        let (_, func, answer) = get_problem(id);
+        let (_, func, _, answer) = get_problem(id);
         let result = func();
         println!("Problem {} should return {}. Returned {}!", id, answer, result);
     }
@@ -90,7 +92,6 @@ fn main() {
 #[cfg(test)]
 seq!(N in 01..=15 {
 #[rstest]
-#[timeout(Duration::new(60, 0))]
 #(
 #[case::problem_~N(N)]
 )*
@@ -106,9 +107,14 @@ seq!(N in 01..=15 {
 // #[case::problem_357(357)]
 #[case::problem_836(836)]
 fn test_problem(#[case] id: u32) -> Result<(), String> {
-    let (_, func, answer) = get_problem(id);
+    let (_, func, slow, answer) = get_problem(id);
+    let start = Instant::now();
     let result = func();
+    let elapsed = start.elapsed();
     assert_eq!(answer, result);
+    if !slow && var_os("TERMUX_VERSION").is_none() {
+        assert!(elapsed <= Duration::new(60, 0));
+    }
     Ok(())
 }
 });
