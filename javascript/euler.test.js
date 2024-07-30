60bc6ccff8f47c7df70d3e82daf976d66d8634cf
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-07-30 18:23:54 -0500

    Fix js mark slow

--- javascript/euler.test.js
+++ javascript/euler.test.js
@@ -2,28 +2,27 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const answers = {
-    1: [require('./src/p0001.js'), 233168],
-    2: [require('./src/p0002.js'), 4613732],
-    4: [require('./src/p0004.js'), 906609],
-    6: [require('./src/p0006.js'), 25164150],
-    8: [require('./src/p0008.js'), 23514624000],
-    9: [require('./src/p0009.js'), 31875000],
-    11: [require('./src/p0011.js'), 70600674],
-    17: [require('./src/p0017.js'), 21124],
-    76: [require('./src/p0076.js'), 190569291],
-    836: [require('./src/p0836.js'), 'aprilfoolsjoke'],
+    1: [require('./src/p0001.js'), false, 233168],
+    2: [require('./src/p0002.js'), false, 4613732],
+    4: [require('./src/p0004.js'), false, 906609],
+    6: [require('./src/p0006.js'), false, 25164150],
+    8: [require('./src/p0008.js'), false, 23514624000],
+    9: [require('./src/p0009.js'), false, 31875000],
+    11: [require('./src/p0011.js'), false, 70600674],
+    17: [require('./src/p0017.js'), false, 21124],
+    76: [require('./src/p0076.js'), true, 190569291],
+    836: [require('./src/p0836.js'), false, 'aprilfoolsjoke'],
 };
-const knownSlow = [76];
 
 for (question in answers) {
     if (answers.hasOwnProperty(question)) {
         const formattedQuestion = `${question}`.padStart(4, '0');
-        const [module, answer] = answers[question];
+        const [module, isSlow, answer] = answers[question];
         const problem = module[`p${formattedQuestion}`];
         describe(`Problem ${formattedQuestion}`, ()=>{
             it(`Should equal ${answer}`, function() {
                 if (typeof this.timeout !== 'undefined') {
-                    if (knownSlow.includes(question)) {
+                    if (isSlow) {
                         this.timeout(Infinity);
                     } else {
                         this.timeout(60 * 1000);
