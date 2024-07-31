f639ffb08a49cb3e97282ed95555aac0f244e510
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-07-30 19:20:30 -0500

    typo

--- javascript/src/p0014.js
+++ javascript/src/p0014.js
@@ -21,7 +21,7 @@
  *
  * NOTE: Once the chain starts the terms are allowed to go above one million.
  *
- * @returns {number}
+ * @return {number}
 */
 exports.p0014 = function() {
     let biggest_seen = 0;
