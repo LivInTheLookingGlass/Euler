6b096f41e476c3799d8784e54f79d0dc6a8c6594
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-08-05 15:37:30 -0500

    dotnet format

--- csharp/Euler/include/utils.cs
+++ csharp/Euler/include/utils.cs
@@ -24,7 +24,8 @@ namespace Euler
 
         public static object GetAnswer(ulong n)
         {
-            foreach (string line in GetDataFileText("answers.csv").Split(new [] { '\r', '\n' })) {
+            foreach (string line in GetDataFileText("answers.csv").Split(new[] { '\r', '\n' }))
+            {
                 var arr = line.Split("\t");
                 if (arr[0] != n.ToString()) continue;
                 if (arr[1] == "str") return arr[3];
