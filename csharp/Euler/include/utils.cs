73e9271f4b731841c4abb885835b9181dc9b8e15
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-08-05 15:20:01 -0500

    fix compilation errors

--- csharp/Euler/include/utils.cs
+++ csharp/Euler/include/utils.cs
@@ -28,8 +28,8 @@ namespace Euler
                 var arr = line.Split("\t");
                 if (arr[0] != n.ToString()) continue;
                 if (arr[1] == "str") return arr[3];
-                if (arr[2] <= 32) return int.Parse(arr[3]);
-                return long.parse(arr[3]);
+                if (int.Parse(arr[2]) <= 32) return int.Parse(arr[3]);
+                return long.Parse(arr[3]);
             }
             throw new IOException();
         }
