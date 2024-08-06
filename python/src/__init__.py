939ce325e640560f9fb5a2cb4790384b8b83ad3e
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-08-06 18:37:38 -0500

    Tweak setup for test-py.html, linting

--- python/src/__init__.py
+++ python/src/__init__.py
@@ -10,7 +10,7 @@ problems: Dict[int, Callable[[], Union[int, str]]] = {}
 
 for i in range(1, 10000):
     try:
-        name = f'p{i:04}'
+        name = f'p{i:04}'  # noqa
         module = import_module(f'.{name}', __name__)
         setattr(modules[__name__], name, module)
         __all__.append(name)
@@ -19,9 +19,9 @@ for i in range(1, 10000):
         pass
 
 
-def run_problems():  # pragma: no cover
+def run_problems() -> None:  # pragma: no cover
     for i, p in problems.items():
         start = perf_counter_ns()
         answer = p()
         stop = perf_counter_ns()
-        print(f'The answer to problem {i:04} is {answer!r} (found in {(stop - start) / 1000:,}μs)')
+        print(f'The answer to problem {i:04} is {answer!r} (found in {(stop - start) / 1000:,}μs)')  # noqa
