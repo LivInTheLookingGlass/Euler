d26ca7b96b04f9f8e8c5c008a7148bc0c9433c2b
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-08-15 21:08:47 -0500

    fix printf

--- cplusplus/src/include/utils.h
+++ cplusplus/src/include/utils.h
@@ -91,7 +91,7 @@ std::string get_data_file(const char *name) {
     const size_t ret_code = fread(buffer, 1, length, file);
     if (ret_code != length) {
         if (feof(file))
-            printf("Error reading %s: unexpected end of file, read %"PRIu64" of %"PRIu64" bytes expected\n", name, (uint64_t)ret_code, (uint64_t)length);
+            printf("Error reading %s: unexpected end of file, read %" PRIu64 " of %" PRIu64 " bytes expected\n", name, (uint64_t)ret_code, (uint64_t)length);
         else if (ferror(file))
             perror("Error reading data file");
     }
