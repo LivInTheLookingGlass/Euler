d7ddd9f50f89e1c79ac440d5f78cda88bcac97a2
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-08-29 22:31:09 -0500

    don't collect very large vector

--- rust/src/p0111.rs
+++ rust/src/p0111.rs
@@ -10,13 +10,20 @@ use crate::include::utils::Answer;
 pub fn p0111() -> Answer {
     let ten_9: u64 = 10_u64.pow(9);
     let ten_10: u64 = 10_u64.pow(10);
-    let primes_int: Vec<u64> = primes_until::<u64>(ten_10).filter(|x| *x > ten_9).collect();
-    let primes_str: Vec<String> = primes_int.iter().map(|p| (*p).to_string()).collect();
     let mut answer: u64 = 0;
-    for digit in 0..=9 {
-        let primes_counts: Vec<usize> = primes_str.iter().map(|s| (*s).bytes().filter(|b| *b == digit + b'0').count()).collect();
-        let n: usize = primes_counts.iter().map(|x| *x).max().unwrap();
-        answer += zip(primes_int.iter(), primes_counts.iter()).filter(|(_i, c)| **c == n).map(|(i, _c)| *i).sum::<u64>();
+    let mut subanswer: Vec<u64> = vec![0; 10];
+    let mut current: Vec<usize> = vec![0; 10];
+    for p in primes_until::<u64>(ten_10).filter(|x| *x > ten_9) {
+        let s = p.to_string();
+        for digit in 0..=9 {
+            let count = s.bytes().filter(|b| *b == digit + b'0').count();
+            if count > current[digit] {
+                current[digit] = count;
+                subanswer[digit] = p;
+            } else if count == current[digit] {
+                subanswer[digit] += p;
+            }
+        }
     }
-    return Answer::Int(answer.into());
+    return Answer::Int(subanswer.into_iter().sum::<u64>().into());
 }
