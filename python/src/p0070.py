2120ffb496d4b57c88d78ce2d721c97600ff4d0e
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-08-31 01:18:18 -0500

    fix linting

--- python/src/p0070.py
+++ python/src/p0070.py
@@ -5,16 +5,20 @@ My time as a TA in the Discrete Mathematics class really paid off here
 
 Problem:
 
-Euler's totient function, $\\phi(n)$ [sometimes called the phi function], is used to determine the number of positive numbers less than or equal to $n$ which are relatively prime to $n$. For example, as $1, 2, 4, 5, 7$, and $8$, are all less than nine and relatively prime to nine, $\\phi(9)=6$.<br>The number $1$ is considered to be relatively prime to every positive number, so $\\phi(1)=1$.
+Euler's totient function, $\\phi(n)$ [sometimes called the phi function], is used to determine the number of positive
+numbers less than or equal to $n$ which are relatively prime to $n$. For example, as $1, 2, 4, 5, 7$, and $8$, are all
+less than nine and relatively prime to nine, $\\phi(9)=6$.<br>The number $1$ is considered to be relatively prime to
+every positive number, so $\\phi(1)=1$.
 
 Interestingly, $\\phi(87109)=79180$, and it can be seen that $87109$ is a permutation of $79180$.
 
-Find the value of $n$, $1 \\lt n \\lt 10^7$, for which $\\phi(n)$ is a permutation of $n$ and the ratio $n/\\phi(n)$ produces a minimum.
+Find the value of $n$, $1 \\lt n \\lt 10^7$, for which $\\phi(n)$ is a permutation of $n$ and the ratio $n/\\phi(n)$
+produces a minimum.
 """
-from functools import reduce
 from itertools import chain
+from functools import reduce
 
-from .lib.primes import primes, prime_factors
+from .lib.primes import prime_factors
 
 
 def totient(n: int) -> int:
