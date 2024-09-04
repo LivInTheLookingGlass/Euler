faacb9691fa06ab6d841a693c4b7513f1e9530c7
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-09-03 20:55:44 -0500

    Modify column widths

--- docs/_static/main.js
+++ docs/_static/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('table.datatable').DataTable({
+        columnDefs: [{ width: 'auto', targets: '_all' }],
         paging: false,
         searching: false,
     });
