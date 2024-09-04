2a40552b842ce15b9c4e6d1bd3c4be30e1aad665
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-09-03 19:34:47 -0500

    Fix file loads

--- docs/conf.py
+++ docs/conf.py
@@ -79,15 +79,15 @@ python_display_short_literal_types = True
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_css_files = [
-    'https://cdn.datatables.net/2.1.5/css/jquery.dataTables.min.css',
-    'https://cdn.datatables.net/fixedheader/4.0.1/css/fixedHeader.dataTables.min.css',
+    'https://cdn.datatables.net/2.1.5/css/jquery.dataTables.css',
+    'https://cdn.datatables.net/fixedheader/4.0.1/css/fixedHeader.dataTables.css',
     'style.css'
 ]
 html_js_files = [
     'https://code.jquery.com/jquery-3.7.1.js',
-    'https://cdn.datatables.net/2.1.5/js/dataTables.min.js',
-    'https://cdn.datatables.net/fixedHeader/4.0.1/js/dataTables.fixedHeader.min.js',
-    'https://cdn.datatables.net/fixedHeader/4.0.1/js/fixedHeader.dataTables.min.js',
+    'https://cdn.datatables.net/2.1.5/js/dataTables.js',
+    'https://cdn.datatables.net/fixedHeader/4.0.1/js/dataTables.fixedHeader.js',
+    'https://cdn.datatables.net/fixedHeader/4.0.1/js/fixedHeader.dataTables.js',
     'main.js',
 ]
 
