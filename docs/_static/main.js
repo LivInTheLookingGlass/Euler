9027fec414a5b9fb462978ab4d5566f74749b1c4
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-09-03 19:28:13 -0500

    Don't allow paging, searching

--- docs/_static/main.js
+++ docs/_static/main.js
@@ -1,6 +1,39 @@
 $(document).ready( function () {
-    $('table.datatable').DataTable({
-        paging: false,
-        searching: false,
+    document.querySelectorAll('.datatable:not(.centertable)').forEach(function(table) {
+        if (window.$ && $.fn.DataTable) {
+            $(table).DataTable({
+                paging: false,
+                searching: false,
+            });
+        }
     });
-} );
+
+    customTables = document.querySelectorAll('.datatable.centertable').forEach(function(table) {
+        if (window.$ && $.fn.DataTable) {
+            var tableInstance = $(table).DataTable({
+                columnDefs: [{
+                    targets: '_all',
+                    orderable: true
+                }],
+                order: [],
+                drawCallback: function() {
+                    this.api().rows().nodes().to$().each(function(index) {
+                        if (index < 6)
+                            $(this).addClass('no-sort');
+                    });
+                },
+                paging: false,
+                searching: false,
+            }).on('order.dt', function() {
+                tableInstance.column(0).order('no-sort').draw();
+            });
+
+            // Custom sorting behavior
+            $.fn.dataTable.ext.order['no-sort'] = function (_, _, dataIndex) {
+                if (dataIndex < 6)
+                    return -1; // Force header and first 5 rows to be at the top
+                return 1;
+            };
+        }
+    });
+});
