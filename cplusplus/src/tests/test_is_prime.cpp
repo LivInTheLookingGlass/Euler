aef147f3605150b0072cb9a5e23c30fd2f0b5fff
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-09-06 21:25:05 -0500

    fix test on non-linux platforms

--- cplusplus/src/tests/test_is_prime.cpp
+++ cplusplus/src/tests/test_is_prime.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdint.h>
 #include "../include/primes.hpp"
 
 #ifndef MAX_PRIME
@@ -7,9 +8,8 @@
 
 int main(int argc, char const *argv[]) {
     // prints number, is_prime(), is_composite(), prime index or -1
-    PrimeGenerator<uintmax_t> pc = primes_until<uintmax_t>(MAX_PRIME);
-    uintmax_t i, p, prev = 0, count = 0;
-    p = pc.next();
+    PrimeGenerator<uint64_t> pc = primes_until<uint64_t>(MAX_PRIME);
+    uint64_t i, prev = 0, count = 0, p = pc.next();
     while (p < MAX_PRIME && pc.has_next()) {
         for (i = prev + 1; i < p; i++)
             std::cout << i << " " << is_prime(i) << " " << is_composite(i) << " -1" << std::endl;
