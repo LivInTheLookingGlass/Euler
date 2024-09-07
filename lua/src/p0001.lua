d74a5d66a05791aba88e161ccf35af26827d6630
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-09-07 00:25:12 -0500

    add "docstrings"

--- lua/src/p0001.lua
+++ lua/src/p0001.lua
@@ -1,3 +1,19 @@
+-- Project Euler Question 1
+-- 
+-- I did it the old-fashioned way in this language
+-- 
+-- Revision 1:
+-- 
+-- I found a closed form solution that works in the general case, so it's about an
+-- order of magnitude faster now.
+-- 
+-- Problem:
+-- 
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+-- get 3, 5, 6 and 9. The sum of these multiples is 23.
+-- 
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
 return {
     solution = function()
         local answer = 0
--- lua/src/p0017.lua
+++ lua/src/p0017.lua
@@ -1,3 +1,20 @@
+-- Project Euler Problem 17
+-- 
+-- The key here was remembering I don't need to return the whole string, just the length
+-- 
+-- Problem:
+-- 
+-- If the numbers 1 to 5 are written out in words: one, two, three, four, five,
+-- then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+-- 
+-- If all the numbers from 1 to 1000 (one thousand) inclusive were written out in
+-- words, how many letters would be used?
+-- 
+-- NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and
+-- forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20
+-- letters. The use of "and" when writing out numbers is in compliance with
+-- British usage.
+
 local function to_string_len(n)
     if n >= 1000
     then
