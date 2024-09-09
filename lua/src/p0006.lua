cbd218352ae157c9f13bbb67c20ca64c60cf48f5
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-09-08 23:28:06 -0500

    Add whitespace for readability

--- lua/src/p0006.lua
+++ lua/src/p0006.lua
@@ -20,11 +20,13 @@ return {
     solution = function()
         local sum = 1
         local sum_of_squares = 1
+
         for i = 2,100,1
         do
             sum = sum + i
             sum_of_squares = sum_of_squares + (i * i)
         end
+
         return (sum * sum) - sum_of_squares;
     end
 }
