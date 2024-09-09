f62aa9c6491beb4c1a36458f306ae57a11085ec0
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-09-08 23:29:19 -0500

    Add whitespace for readability

--- lua/src/p0009.lua
+++ lua/src/p0009.lua
@@ -15,15 +15,19 @@
 return {
     solution = function()
         local c = 3
+
         while true
         do
             local c_square = c * c
+
             for b = 2,c,1
             do
                 local b_square = b * b
+
                 for a = 1,b,1
                 do
                     local a_square = a * a
+
                     if a_square + b_square == c_square and a + b + c == 1000
                     then
                         return a * b * c
