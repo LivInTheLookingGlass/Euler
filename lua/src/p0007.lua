d4845c46d9f3f6e397239e980f62f1d644dade3b
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-09-10 23:33:47 -0500

    Debug p7, mark p3 as solved

--- lua/src/p0007.lua
+++ lua/src/p0007.lua
@@ -2,6 +2,10 @@
 --
 -- Problem:
 --
+-- By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
+-- the 6th prime is 13.
+--
+-- What is the 10 001st prime number?
 
 local primes = loadlib("primes").primes
 
@@ -14,6 +18,7 @@ return {
         repeat
             idx = idx + 1
             num = pgen.next()
+            print(num)
         until idx > 10001
 
         return num
