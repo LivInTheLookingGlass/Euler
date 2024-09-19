c3136e214c4ca6af2014e07a946c4f4e2566c877
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-09-18 21:52:14 -0500

    Add time tracking, p836 validation to test runner

--- fortran/test.f90
+++ fortran/test.f90
@@ -36,7 +36,6 @@ program test
   /)
 
   call process_problems(problem_ids, answers, long_runtime)
-
   deallocate(problem_ids, answers, long_runtime)
 
 contains
@@ -45,7 +44,8 @@ contains
     integer, dimension(:), intent(in) :: problem_ids
     integer, dimension(:), intent(in) :: answers
     logical, dimension(:), intent(in) :: long_runtime
-    integer :: i, answer
+    integer :: i, answer, first_count, second_count, count_rate, count_max
+    real :: time_elapsed
 
     ! Loop through each problem
     do i = 1, size(problem_ids)
@@ -53,13 +53,20 @@ contains
       if (long_runtime(i)) then
         print *, "  This problem will take more than 60 seconds."
       end if
+      call system_clock(first_count, count_rate, count_max)
       answer = select_function(problem_ids(i))
+      call system_clock(second_count, count_rate, count_max)
       if (answers(i) /= answer) then
-        print *, "Error: problem ", problem_ids(i), " failed!"
-        print *, "Expected Answer   : ", answers(i)
-        print *, "Solution returned : ", answer
+        print *, "  Error: problem ", problem_ids(i), " failed!"
+        print *, "  Expected Answer  : ", answers(i)
+        print *, "  Solution returned: ", answer
       end if
-      print *, "  Completed: ", problem_ids(i)
+      time_elapsed = real((second_count - first_count) % count_max) / real(count_rate)
+      if (.NOT. long_runtime(i) .AND. time_elapsed > 60.0) then
+        print *, "  Error: problem ", problem_ids(i), " timed out!"
+        print *, "  Solution took    : ", time_elapsed, "s"
+      end if
+      print *, "  Completed        : ", problem_ids(i), "in ", time_elapsed, "s"
     end do
   end subroutine process_problems
 
@@ -79,7 +86,11 @@ contains
       case (836)
         allocate(str(14))
         str = p0836()
-        select_function = 0
+        if (str /= "aprilfoolsjoke") then
+          select_function = 0
+        else
+          select_function = -2
+        end if
         deallocate(str)
       case default
         print *, "Unknown problem ID!"
