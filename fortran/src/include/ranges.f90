79e3ea608b8032d2adf7d0dc47042bf8014ae405
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-09-29 15:47:03 -0500

    Solve p28 in fortran (3)

--- fortran/src/include/ranges.f90
+++ fortran/src/include/ranges.f90
@@ -11,24 +11,25 @@ contains
 
     integer function range_entry4(start, stop, step, idx) result(answer)
         integer, intent(in) :: start, stop, step, idx
-        integer :: length
+        integer :: length, l_idx
 
+        l_idx = idx
         if (step > 0 .and. start < stop) then
             length = 1 + (stop - 1 - start) / step
         elseif (step < 0 .and. start > stop) then
             length = 1 + (start - 1 - stop) / (-step)
         end if
 
-        if (idx < 0) then
-            idx = length + idx
+        if (l_idx < 0) then
+            l_idx = length + l_idx
         end if
 
-        if (idx < 0 .or. idx >= length) then
+        if (l_idx < 0 .or. l_idx >= length) then
             answer = huge(kind(answer))  ! error
             return
         end if
 
-        answer = range_entry3(start, step, idx)
+        answer = range_entry3(start, step, l_idx)
     end function
 
 end module ranges
