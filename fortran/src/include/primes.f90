5ec79d8f2e8ce3f7f0cdf8a5114f953e158321c4
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-09-30 23:22:15 -0500

    Attempt p7 in fortran (5)

--- fortran/src/include/primes.f90
+++ fortran/src/include/primes.f90
@@ -75,13 +75,13 @@ contains
     ! Function to set a bit to 0
     subroutine clear_prime_bit(num)
         integer, intent(in) :: num
-        is_prime(num / bits_per_int) = bitand(is_prime(num / bits_per_int), ieor(-1, 2**(mod(num, bits_per_int))))
+        is_prime(num / bits_per_int) = is_prime(num / bits_per_int) .and. ieor(-1, 2**(mod(num, bits_per_int)))
     end subroutine clear_prime_bit
 
     ! Function to check if a bit is set
     logical function get_prime_bit(num) result(bit)
         integer, intent(in) :: num
-        bit = (btest(is_prime(num / bits_per_int), mod(num, bits_per_int)) .neqv. .false.)
+        bit = btest(is_prime(num / bits_per_int), mod(num, bits_per_int)) .neqv. .false.
     end function get_prime_bit
 
 end module primes
