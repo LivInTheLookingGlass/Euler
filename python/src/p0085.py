00f751fd7e7a73a2ff99c0524127b99e3ccf4d84
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-10-19 23:05:12 -0500

    Fix flake8

--- python/src/p0085.py
+++ python/src/p0085.py
@@ -7,7 +7,8 @@ By counting carefully it can be seen that a rectangular grid measuring 3 by 2 co
 
 .. image:: https://projecteuler.net/resources/images/0085.png
 
-Although there exists no rectangular grid that contains exactly two million rectangles, find the area of the grid with the nearest solution.
+Although there exists no rectangular grid that contains exactly two million rectangles, find the area of the grid with
+the nearest solution.
 """
 from math import ceil, sqrt
 
